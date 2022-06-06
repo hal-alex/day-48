abb999234e8b4c905a4a001f1cbea82e316f5cc0
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-06-06 17:16:17 +0100

    Scraping Python site

--- main.py
+++ main.py
@@ -21,26 +21,3 @@
 # print(events_calendar)
 #
 #
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-s = Service('E:\Alex\Downloads\chromedriver_win32\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
-
-driver.get("https://www.python.org/")
-
-event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
-event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
-
-events_calendar = {}
-
-for n in range(len(event_times)):
-    events_calendar[n] = {
-        "time": event_times[n].text,
-        "name": event_names[n].text
-    }
-
-print(events_calendar)
-
-
